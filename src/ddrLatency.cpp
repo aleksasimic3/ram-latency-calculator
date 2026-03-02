75d3386d6ffe3360aee571b5c770ae9d083a7bce
Aleksa Simić <aleksasimic8@gmail.com>
2026-03-02 03:17:00 +0100

    Added printing of most common DDR4 speeds when program is run without arguments.

--- src/ddrLatency.cpp
+++ src/ddrLatency.cpp
@@ -5,11 +5,19 @@
 #include "ramInfo.h"
 
 void printUsage();
+void printCommonValues();
 
 //make gui
 int main(int argc, char *argv[]) {
 
+	if(argc == 1) {
+		printCommonValues();
+		printUsage();
+		return 0;
+	}
+
 	if(argc < 3 || (argc - 1) % 2 != 0) {
+		std::cout << "Incorrect input!\n";
 		printUsage();
 		return 0;
 	}
@@ -19,10 +27,10 @@ int main(int argc, char *argv[]) {
 	for(int i = 1; i < argc; i += 2) {
 		
 		try {
-			s = std::stoul(argv[i], NULL, 10); //why stol when s is of type unsigned???
+			s = std::stoul(argv[i], NULL, 10);
 			cl = std::stoul(argv[i+1], NULL, 10);
 		}
-		catch(...) {
+		catch(...) { //name this exception
 			//skip this pair
 			continue;
 		}
@@ -41,7 +49,19 @@ int main(int argc, char *argv[]) {
 }
 
 void printUsage() {
-	std::cout << "Incorrect Input!\n" << 
-					"Usage: ddrLatency.exe [speed] [casLatency] ([speed2] [casLatency2] ...)\n" <<
+	std::cout <<"Usage: ddrLatency.exe [speed] [casLatency] ([speed2] [casLatency2] ...)\n" <<
 					"\nPrints out calculated total latency of a given memory module or list of modules sorted in ascending order.";
+}
+
+void printCommonValues() {
+	//2133 15 2400 16 2666 18 3200 22 3200 16 3600 18
+	std::vector<std::pair<unsigned, unsigned>> values = {{2311, 15}, {2400, 16}, {2666, 18}, {3200, 22}, {3200, 16}, {3600, 18}};
+
+	std::cout << "Most common DDR4 values:\n";
+	for(auto specs: values) {
+		ramInfo info(specs.first, specs.second);
+		std::cout << '\t' << info << '\n';
+	}
+
+	std::cout << '\n';
 }
